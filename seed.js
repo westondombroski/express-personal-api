e6444fe0d94bf424e3a33da8a632312e1fc1aca1
westondombroski <westondombroski@gmail.com>
2017-02-12 23:32:45 -0800

    fixed pics??

--- seed.js
+++ seed.js
@@ -1,13 +1,10 @@
-//This file allows us to seed our application with data
-//simply run: `node seed.js` from the root of this project folder.
-
 var db = require('./models');
 
 var drummers_list = [
   {
   name: "elvin jones",
   rank: 1,
-  image: "/public/images/elvin.jpg",
+  image: "/images/elvin.jpg",
   life: "9/9/1927 - 5/18/2004",
   url: "https://www.youtube.com/watch?v=JWQjqWiGzh8",
   isAlive: false
@@ -15,7 +12,7 @@ var drummers_list = [
   {
   name: "tony williams",
   rank: 2,
-  image: "/public/images/tony.jpg",
+  image: "/images/tony.jpg",
   life: "12/12/1945 - 2/23/1997",
   url: "https://www.youtube.com/watch?v=QcKgpPvzi18",
   isAlive: false
@@ -23,7 +20,7 @@ var drummers_list = [
   {
   name: "buddy rich",
   rank: 3,
-  image: "/public/images/buddy.jpg",
+  image: "/images/buddy.jpg",
   life: "9/30/1917 - 4/2/1987",
   url: "https://www.youtube.com/watch?v=VJh9W3Gcpmo",
   isAlive: false
@@ -31,7 +28,7 @@ var drummers_list = [
   {
   name: "jack dejohnette",
   rank: 4,
-  image: "/public/images/jack.jpg",
+  image: "/images/jack.jpg",
   life: "9/9/1942 - alive",
   url: "https://www.youtube.com/watch?v=wjCguSiMXUI",
   isAlive: true
@@ -39,17 +36,20 @@ var drummers_list = [
   {
   name: "jeff hamilton",
   rank: 5,
-  image: "/public/images/jeff.jpg",
+  image: "/images/jeff.jpg",
   life: "9/5/1953 - alive",
   url: "https://www.youtube.com/watch?v=-xfw-BZt8pU",
   isAlive: true
   }
 ];
 
-db.Drummer.create(drummers_list, function(err, drummers){
-  if (err){
-    return console.log("Error:", err);
-  }
-  console.log("Created data")
-  process.exit();
+db.Drummer.remove({}, function(err, drummers) {
+  console.log("removed all drummers")
+  db.Drummer.create(drummers_list, function(err, drummers){
+    if (err){
+      return console.log("Error:", err);
+    }
+    console.log("Created data")
+    process.exit();
+  });
 });
